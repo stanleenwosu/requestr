98fdeec42208917263e2288265f1a33d889350ab
Stanley Nwosu <stanleenwosu@yahoo.com>
2021-08-19 11:37:52 +0100

    Fixed Issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,12 +5,13 @@
  */
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDhSrg59-gVD4JFI8K0G761VK38rsh54XY",
-    authDomain: "requestr-f4a36.firebaseapp.com",
-    projectId: "requestr-f4a36",
-    storageBucket: "requestr-f4a36.appspot.com",
-    messagingSenderId: "626948144605",
-    appId: "1:626948144605:web:c1ac2e6424902445beca34"
-  }
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhSrg59-gVD4JFI8K0G761VK38rsh54XY",
+  authDomain: "requestr-f4a36.firebaseapp.com",
+  projectId: "requestr-f4a36",
+  storageBucket: "requestr-f4a36.appspot.com",
+  messagingSenderId: "626948144605",
+  appId: "1:626948144605:web:c1ac2e6424902445beca34"
+}
